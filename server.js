4cb00b6375d5cf420115639a85f250a6478a18aa
DMQuan03 <123900871+DMQuan03@users.noreply.github.com>
2024-04-04 16:19:19 +0700

    new

--- server.js
+++ server.js
@@ -2,65 +2,17 @@ const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
 const cors = require("cors")
-const db = require("./src/config/mgdb.config")
-const Routers = require('./src/routes/index')
-const socket = require("socket.io")
-const CMT = require("./src/model/comment")
-const User = require("./src/model/user")
-const Movie = require("./src/model/movie")
-dotenv.config()
+const ctrls = require("./controller/botnet")
 const PORT = process.env.PORT || 6578
 
-app.use(cors())
-db.connect()
+dotenv.config()
+app.use(cors("*"))
 app.use(express.json())
-Routers(app)
 
 
-
-const server = app.listen(PORT , () => {
-    console.log("server is running");
-})
-
-const io = socket(server, {
-    cors : {
-        origin : "*",
-        credentials : true,
-        methods : ["GET", "PUT", 'PATCH', 'DELETE', 'POST']
-    }
-})
+app.get("/", ctrls.get_data )
 
 
-io.on("connection", (socket) => {
-
-    socket.on("join_room", (data) => {
-        for (let i of socket.rooms) {
-            socket.leave(i)
-        }
-        console.log("user-join-room" + data.id);
-        socket.join(data.id)
-    })
-
-    socket.on("leave_room", (data) => {
-        socket.leave(data.id)
-        console.log("user-leave-room" + data.id);
-    })
-
-    socket.on("user-comment", async(data) => {
-        try {
-            socket.join(data.idRoom)
-            const newComment = await new CMT({
-                text : data.text,
-                idRoom : data.idRoom,
-                userId : data.idUser
-            })
-            await newComment.save()
-            await Movie.findByIdAndUpdate({_id : data.idRoom}, { $addToSet : { comment : newComment._id }})
-            await newComment.populate("userId", "name avatar _id")
-            io.sockets.to(data.idRoom).emit("user-comment-movie", newComment)
-        } catch (error) {
-            console.log(error);
-            return 0
-        }
-    })
+app.listen(PORT, ()=> {
+    console.log(`server is running port ${PORT}`)
 })
--- src/routes/utils.js
+++
@@ -1,6 +0,0 @@
-const Router = require("express").Router()
-const ctrls = require("../controller/utils")
-
-Router.get("/comment/:_id", ctrls.getComment )
-
-module.exports = Router
